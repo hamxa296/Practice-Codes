744ec12bcc5c9958e8e1dce5d4bb7a10765a249a
hamxa296 <hamzzarif296@gmail.com>
2025-03-16 14:25:13 +0500

    commit

--- OOP/vehiclereg.cpp
+++ OOP/vehiclereg.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 using namespace std;
+
 enum vehicle_type
 {
     Bike = 1,
     Car,
     Truck
 };
+string getvehtype(vehicle_type vt);
 union unique_details
 {
     int seats;
@@ -15,15 +17,34 @@ union unique_details
 struct Vehicle
 {
     int Vehicle_Id;
-    string Vehicle_Type;
+    vehicle_type Vehicle_Type;
     int Vehicle_Details;
 };
 void Display(struct Vehicle veh)
 {
-    cout << "---------------------------Details for Vehicle Entered--------------------------------";
-    cout << "Vehicle ID: " << veh.Vehicle_Id;
-    cout << "Vehicle Type: " << veh.Vehicle_Type;
-    cout << "Vehicle Unique detail: " << veh.Vehicle_Details;
+    cout << "-------------Details for Vehicle Entered-------------------\n\n";
+    cout << "Vehicle ID: " << veh.Vehicle_Id << endl;
+    cout << "Vehicle Type: " << getvehtype(veh.Vehicle_Type) << endl;
+    cout << "Vehicle Unique detail: " << veh.Vehicle_Details << endl;
+}
+string getvehtype(vehicle_type vt)
+{
+    switch (vt)
+    {
+    case Bike:
+        return "Bike";
+        break;
+    case Car:
+        return "Car";
+        break;
+    case Truck:
+        return "Truck";
+        break;
+
+    default:
+        return "Vehicle type not identifed! ";
+        break;
+    }
 }
 int main()
 {
@@ -43,25 +64,25 @@ int main()
         veh1.Vehicle_Type = Bike;
         break;
     case Car:
-        cout << "Enter the seats of the bike: " << endl;
-        cin >> unique.seats;
+        cout << "Enter the engine capacity of the car: " << endl;
+        cin >> unique.engine_capacity;
         cout << "Enter the vehicle ID: " << endl;
         cin >> veh1.Vehicle_Id;
         veh1.Vehicle_Details = unique.seats;
-        veh1.Vehicle_Type = Bike;
+        veh1.Vehicle_Type = Car;
         break;
     case Truck:
-        cout << "Enter the seats of the bike: " << endl;
-        cin >> unique.seats;
+        cout << "Enter the load capacity of the truck: " << endl;
+        cin >> unique.max_load_capacity;
         cout << "Enter the vehicle ID: " << endl;
         cin >> veh1.Vehicle_Id;
         veh1.Vehicle_Details = unique.seats;
-        veh1.Vehicle_Type = Bike;
+        veh1.Vehicle_Type = Truck;
         break;
     default:
         cout << "Vehicle not identified! :/" << endl;
         break;
     }
-
+    Display(veh1);
     return 0;
 }
