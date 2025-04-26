7a6889b3d113cff87a421cca01e972eb04156c4b
hamxa296 <hamzzarif296@gmail.com>
2025-04-26 14:36:57 +0500

    Create enum.cpp

---
+++ OOP_basics/enum.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+enum color
+{
+    white = 0,
+    yellow = 50,
+    red = 100
+
+};
+int main()
+{
+    string colour;
+    color clr;
+    cout << "Enter colour: " << endl;
+    cin >> colour;
+    
+    return 0;
+}
